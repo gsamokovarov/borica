584febf72b07cd83290fa8ca317a8ef722952b4d
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-02-05 20:21:58 +0200

    Prepare the borica tests

--- test/borica_test.rb
+++ test/borica_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class BoricaTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Borica::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
+  # TODO(genadi): Test out the configuration, once we get there, or
+  # whatever else is at the Borica module level, once we get there.
 end
